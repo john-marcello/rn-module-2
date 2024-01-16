fd0dca4cbc14a48cd04a3ef046ca70d726da9d4e
John Marcello <jm@#jmarcello.com>
2024-01-15 17:20:26 -0700

    feat(add): Screen setup

---
+++ components/ButtonPrimary.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function ButtonPrimary({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text> 
+    </View> 
+  );
+}
+
+export default ButtonPrimary;
