3fff39a62f48cdaaeaf26f90a0cec4e861e966d9
John Marcello <jm@#jmarcello.com>
2024-01-15 17:45:41 -0700

    feat(add): Add custom button styles with shadows

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import ButtonPrimary from "../components/ButtonPrimary";
 
 function StartGame({}) {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <ButtonPrimary>Confirm</ButtonPrimary>
             <ButtonPrimary>Reset</ButtonPrimary>
@@ -12,3 +12,20 @@ function StartGame({}) {
 }
 
 export default StartGame;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4, // android only
+    shadowColor: '#000000', // ios only
+    shadowOffset: { width: 0, height: 2 }, // ios only
+    shadowRadius: 6, // ios only
+    shadowOpacity: 0.25, // ios only
+  },
+});
